acf973bf24c514f398dfeecd4b0cfbc0158df1be
harleysherman <harsatr11@icloud.com>
2024-01-31 21:06:09 -0600

    added employee table, silly comma

--- db/schema.sql
+++ db/schema.sql
@@ -27,7 +27,7 @@ CREATE TABLE employee (
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL
+    ON DELETE SET NULL,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
