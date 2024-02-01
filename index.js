73f65bf0d40a547da26e45211513249d1f27a356
harleysherman <harsatr11@icloud.com>
2024-01-31 22:10:43 -0600

    added view all roles, departments, and employess in index.js

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const db = mysql.createConnection(
         user: 'root',
         // MySQL password
         password: 'kittens123',
-        database: 'courses_db'
+        database: 'employee_db'
     },
-    console.log(`Connected to the courses_db database.`)
+    console.log(`Connected to the employee_db database.`)
 );
 
 //asks user questions about what text, font color, shape and shape color they want for their logo
@@ -24,27 +24,66 @@ function promptMenu() {
             type: 'list',
             message: 'What would you like to do?',
             name: 'actionChosen',
-            choices: ['View all departments', 'View all roles', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
         },)
     .then((response) => {
         console.log(response);
-    //TODO view all departments
-    //if(response.actionChosen === "View all departments") {
-    //         db.query('WRITE QUERY TO VIEW ALL DEPTS', 3, (err, result) => {
-    //             if (err) {
-    //                 console.log(err);
-    //             } else {
-    //                 console.log(result);
-    //                 promptMenu();
-    //             }
-    //         })
-    //}
-
-    //     //TODO view all roles
-
-    //     //TODO view all employees
-
-    //     //TODO add a department
+        //TODO view all departments
+        if(response.actionChosen === "View all departments") {
+            db.query('SELECT * FROM department ', 3, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.table(result);
+                    promptMenu();
+                }
+            })  
+        } else if (response.actionChosen === "View all roles") {
+            //TODO view all roles
+            db.query('SELECT * FROM role', 3, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.table(result);
+                    promptMenu();
+                }
+            })  
+        } else if (response.actionChosen === "View all employees") {
+            //TODO view all employees
+            db.query('SELECT * FROM employee', 3, (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.table(result);
+                    promptMenu();
+                }
+            })  
+        } else if (response.actionChosen === "Add a department") {
+            //TODO add a department
+            console.log(response);
+            db.query('INSERT INTO department (department_name) VALUES (?)', response, (err, result) => {
+                //console.log("we've started the query");
+                if (err) {
+                    //console.log("we printed the error instead");
+                    console.log(err);
+                } else {
+                    console.log("we're here to add a department");
+                    inquirer
+                    .prompt(
+                        {
+                            type: 'input',
+                            message: 'What is the name of the department you want to add?',
+                            name: 'departmentAdded',
+                       }
+                    )
+                    console.table(result);
+                    promptMenu();
+                }
+            })  
+        }
+    })
+} 
+    //     /
 
     //     //TODO add a role
 
@@ -52,8 +91,6 @@ function promptMenu() {
 
     //     //TODO update an employee role
     //     //don't add promptMenu() here, this is the end
- })
-}
 // inquirer.prompt ()
 // // Hardcoded query: DELETE FROM course_names WHERE id = 3;
 // db.query(`SELECT * FROM department`, 3, (err, result) => {
@@ -69,4 +106,3 @@ function promptMenu() {
 // });
 
 promptMenu();
-
